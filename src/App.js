9d8f67bd4557405e3d58897ddc87f5eeeae15a93
bharat <bharat@C40LAP-10381.india.rsystems.com>
2023-07-11 11:06:11 +0530

    file update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './main.scss';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Bharat Negi
         </a>
       </header>
     </div>
