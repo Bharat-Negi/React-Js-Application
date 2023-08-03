44fd498e2975252100cf23684f4c428da1cb93f1
bharat <bharat@C40LAP-10381.india.rsystems.com>
2023-08-03 09:46:39 +0530

    product list dynamic

--- src/app/data-files/product-list.jsx
+++ src/app/data-files/product-list.jsx
@@ -2,39 +2,66 @@
 const ProductListFile = [
     {
         id : '1',
-        cimage : 'images/product-2.jpg',
-        cname : 'Product One',
-        cprotext : 'Some quick example product one',
-        cproductlink : 'product one item',
-        clink : 'https://www.google.com/',
-        canother : 'https://www.facebook.com/'
+        pimage : 'images/shop_01.jpg',
+        pname : 'Product Name 1',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
     },
     {
         id : '2',
-        cimage : 'images/product-3.jpg',
-        cname : 'Product Two',
-        cprotext : 'Some quick example product two',
-        cproductlink : 'product two item',
-        clink : 'https://www.google.com/',
-        canother : 'https://www.facebook.com/'
+        pimage : 'images/shop_02.jpg',
+        pname : 'Product Name 2',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
     },
     {
         id : '3',
-        cimage : 'images/product-4.jpg',
-        cname : 'Product Three',
-        cprotext : 'Some quick example product three',
-        cproductlink : 'product three item',
-        clink : 'https://www.google.com/',
-        canother : 'https://www.facebook.com/'
+        pimage : 'images/shop_03.jpg',
+        pname : 'Product Name 3',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
     },
     {
         id : '4',
-        cimage : 'images/product-5.jpg',
-        cname : 'Product Four',
-        cprotext : 'Some quick example product four',
-        cproductlink : 'product four item',
-        clink : 'https://www.google.com/',
-        canother : 'https://www.facebook.com/'
+        pimage : 'images/shop_04.jpg',
+        pname : 'Product Name 4',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
+    },
+    {
+        id : '5',
+        pimage : 'images/shop_05.jpg',
+        pname : 'Product Name 5',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
+    },
+    {
+        id : '6',
+        pimage : 'images/shop_06.jpg',
+        pname : 'Product Name 6',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
+    },
+    {
+        id : '7',
+        pimage : 'images/shop_07.jpg',
+        pname : 'Product Name 7',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
+    },
+    {
+        id : '8',
+        pimage : 'images/shop_08.jpg',
+        pname : 'Product Name 8',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
+    },
+    {
+        id : '9',
+        pimage : 'images/shop_09.jpg',
+        pname : 'Product Name 9',
+        protext : 'M/L/X/XL',
+        price: '$250.00'
     }
 ];
 
